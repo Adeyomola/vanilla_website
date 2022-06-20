237c89d253e81363d265a3b3f4bef3a52c29ba98
Adeyomola <kayzeel@yahoo.com>
2022-06-20 18:15:51 +0100

    Add visitor's welcome message using DOM Modification

--- script/index.js
+++ script/index.js
@@ -1,2 +1,17 @@
+let visitor = document.createElement("div");
+
 let visitorsName = prompt("Please enter a name");
-alert("Welcome" + " " + visitorsName)
+alert("Welcome" + " " + visitorsName);
+
+visitor.innerText = "Welcome " + visitorsName;
+visitor.style.color = "black";
+visitor.style.fontSize = "20px";
+visitor.style.fontFamily = "Candara"
+visitor.style.width = "80%";
+visitor.style.marginLeft = "auto";
+visitor.style.marginRight = "auto"
+visitor.style.padding = "10px"
+visitor.style.textAlign = "center"
+
+const nav = document.querySelector("nav")
+nav.insertAdjacentElement("afterend", visitor)
