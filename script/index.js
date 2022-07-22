9bb728412cf4ed395c1b3f6d3fdd5c32a180e0e1
Adeyomola <kayzeel@yahoo.com>
2022-07-22 10:58:34 +0100

    Added new links to the frontend page

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 const hamburger = document.getElementById("hamburger");
 let nav = document.querySelector("nav");
 const layers = document.querySelectorAll(".hamburger span");
+const all = document.documentElement;
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("shownav");
   layers.forEach((layer) => layer.classList.toggle("hamburgerActive"));
 });
-
